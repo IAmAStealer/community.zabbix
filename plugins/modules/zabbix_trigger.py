8c7b5c535b9e1ac3a93842be6ebfb5bfaa7d469b
IAmAStealer <alexisstealer@gmail.com>
2026-03-08 16:08:18 +0100

    changed that sanitize function
    I fixed something a while ago without having time. Tried a fix on my phone on the way to holidays with AI helping to write on supervised specific work. It removed some things so i should reread later for what to keep

--- plugins/modules/zabbix_trigger.py
+++ plugins/modules/zabbix_trigger.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
-
 
 DOCUMENTATION = r'''
 ---
@@ -156,7 +153,6 @@ extends_documentation_fragment:
 '''
 
 EXAMPLES = r'''
-
 # If you want to use Username and Password to be authenticated by Zabbix Server
 - name: Set credentials to access Zabbix Server API
   ansible.builtin.set_fact:
@@ -164,21 +160,19 @@ EXAMPLES = r'''
     ansible_httpapi_pass: zabbix
 
 # If you want to use API token to be authenticated by Zabbix Server
-# https://www.zabbix.com/documentation/current/en/manual/web_interface/frontend_sections/administration/general#api-tokens
 - name: Set API token
   ansible.builtin.set_fact:
     ansible_zabbix_auth_key: 8ec0d52432c15c91fcafe9888500cf9a607f44091ab554dbee860f6b44fac895
 
 # Create ping trigger on example_host
 - name: create ping trigger
-  # set task level variables as we change ansible_connection plugin here
   vars:
     ansible_network_os: community.zabbix.zabbix
     ansible_connection: httpapi
     ansible_httpapi_port: 443
     ansible_httpapi_use_ssl: true
     ansible_httpapi_validate_certs: false
-    ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_zabbix_url_path: 'zabbixeu'
     ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_trigger:
     name: agent_ping
@@ -192,14 +186,13 @@ EXAMPLES = r'''
 
 # Create ping trigger on example_template
 - name: create ping trigger
-  # set task level variables as we change ansible_connection plugin here
   vars:
     ansible_network_os: community.zabbix.zabbix
     ansible_connection: httpapi
     ansible_httpapi_port: 443
     ansible_httpapi_use_ssl: true
     ansible_httpapi_validate_certs: false
-    ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_zabbix_url_path: 'zabbixeu'
     ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_trigger:
     name: agent_ping
@@ -213,14 +206,13 @@ EXAMPLES = r'''
 
 # Add tags to the existing Zabbix trigger
 - name: update ping trigger
-  # set task level variables as we change ansible_connection plugin here
   vars:
     ansible_network_os: community.zabbix.zabbix
     ansible_connection: httpapi
     ansible_httpapi_port: 443
     ansible_httpapi_use_ssl: true
     ansible_httpapi_validate_certs: false
-    ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_zabbix_url_path: 'zabbixeu'
     ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_trigger:
     name: agent_ping
@@ -237,14 +229,13 @@ EXAMPLES = r'''
 
 # delete Zabbix trigger
 - name: delete ping trigger
-  # set task level variables as we change ansible_connection plugin here
   vars:
     ansible_network_os: community.zabbix.zabbix
     ansible_connection: httpapi
     ansible_httpapi_port: 443
     ansible_httpapi_use_ssl: true
     ansible_httpapi_validate_certs: false
-    ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_zabbix_url_path: 'zabbixeu'
     ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_trigger:
     name: agent_ping
@@ -252,14 +243,13 @@ EXAMPLES = r'''
     state: absent
 
 - name: Rename Zabbix trigger
-  # set task level variables as we change ansible_connection plugin here
   vars:
     ansible_network_os: community.zabbix.zabbix
     ansible_connection: httpapi
     ansible_httpapi_port: 443
     ansible_httpapi_use_ssl: true
     ansible_httpapi_validate_certs: false
-    ansible_zabbix_url_path: "zabbixeu"  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_zabbix_url_path: "zabbixeu"
     ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_trigger:
     name: agent_ping
@@ -270,116 +260,81 @@ EXAMPLES = r'''
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-
 from ansible_collections.community.zabbix.plugins.module_utils.base import ZabbixBase
 import ansible_collections.community.zabbix.plugins.module_utils.helpers as zabbix_utils
 
 
 class Trigger(ZabbixBase):
 
-    PRIORITY_TYPES = {'not_classified': 0,
-                      'information': 1,
-                      'warning': 2,
-                      'average': 3,
-                      'high': 4,
-                      'disaster': 5}
+    PRIORITY_TYPES = {
+        'not_classified': 0,
+        'information': 1,
+        'warning': 2,
+        'average': 3,
+        'high': 4,
+        'disaster': 5
+    }
 
-    RECOVERY_MODES = {'expression': 0,
-                      'recovery_expression': 1,
-                      'none': 2}
+    RECOVERY_MODES = {
+        'expression': 0,
+        'recovery_expression': 1,
+        'none': 2
+    }
 
     def get_triggers(self, trigger_name, host_name, template_name):
-        if host_name is not None:
-            host = host_name
-        else:
-            host = template_name
-        triggers = []
+        host = host_name if host_name is not None else template_name
         try:
-            triggers = self._zapi.trigger.get({'filter': {'description': trigger_name, 'host': host}, "selectDependencies": "extend", "selectTags": "extend"})
+            return self._zapi.trigger.get({'filter': {'description': trigger_name, 'host': host}, "selectDependencies": "extend", "selectTags": "extend"})
         except Exception as e:
             self._module.fail_json(msg="Failed to get trigger: %s" % e)
-        return triggers
 
     def sanitize_params(self, name, params, desc=None, dependencies=None):
         params['description'] = name
         if desc is not None:
             params['comments'] = desc
-        if 'severity' in params:
-            params['priority'] = params['severity']
-            params.pop("severity")
-        if 'priority' in params:
-            priority_id = self.PRIORITY_TYPES[params['priority']]
-            params['priority'] = priority_id
+
+        severity_str = params.pop('severity', params.get('priority'))
+        if severity_str in self.PRIORITY_TYPES:
+            params['priority'] = self.PRIORITY_TYPES[severity_str]
+
         if 'enabled' in params:
-            if params['enabled']:
-                params['status'] = 'enabled'
-            else:
-                params['status'] = 'disabled'
-            params.pop("enabled")
-        if 'status' in params:
-            status = params['status']
-            if status == 'enabled':
-                params['status'] = 0
-            elif status == 'disabled':
-                params['status'] = 1
-            else:
-                self._module.fail_json(msg="Status must be 'enabled' or 'disabled', got %s" % status)
+            params['status'] = 0 if params.pop('enabled') else 1
+        elif 'status' in params:
+            params['status'] = 0 if params['status'] == 'enabled' else 1
+
         if 'generate_multiple_events' in params:
-            multiple_event_type = params['generate_multiple_events']
-            if multiple_event_type:
-                params['type'] = 1
-            else:
-                params['type'] = 0
-            del params['generate_multiple_events']
-        if 'recovery_mode' in params:
-            recovery_mode_id = self.RECOVERY_MODES[params['recovery_mode']]
-            params['recovery_mode'] = recovery_mode_id
-        if 'correlation_mode' in params:
-            correlation_mode = params['correlation_mode']
-            if correlation_mode == 'all':
-                params['correlation_mode'] = 0
-            elif correlation_mode == 'tag':
-                params['correlation_mode'] = 1
-            else:
-                self._module.fail_json(msg="correlation_mode must be all or tag, got %s" % correlation_mode)
+            params['type'] = int(bool(params.pop('generate_multiple_events')))
+            
         if 'manual_close' in params:
-            manual_close = params['manual_close']
-            if manual_close:
-                params['manual_close'] = 1
-            else:
-                params['manual_close'] = 0
-        if dependencies is not None:
+            params['manual_close'] = int(bool(params['manual_close']))
+
+        if 'recovery_mode' in params and params['recovery_mode'] in self.RECOVERY_MODES:
+            params['recovery_mode'] = self.RECOVERY_MODES[params['recovery_mode']]
+
+        if 'correlation_mode' in params:
+            params['correlation_mode'] = 0 if params['correlation_mode'] == 'all' else 1
+
+        if dependencies:
             params['dependencies'] = []
-            for dependency in dependencies:
-                host_name = None
-                template_name = None
-                if 'host_name' in dependency:
-                    host_name = dependency
-                elif 'template_name' in dependency:
-                    template_name = dependency
-                else:
-                    self._module.fail_json(msg="Each dependency must contain either the host_name or the template_name")
-                triggers = self.get_triggers(dependency['name'], host_name, template_name)
-                for trigger in triggers:
-                    params['dependencies'].append({'triggerid': trigger['triggerid']})
+            for dep in dependencies:
+                triggers = self.get_triggers(dep['name'], dep.get('host_name'), dep.get('template_name'))
+                params['dependencies'].extend([{'triggerid': t['triggerid']} for t in triggers])
 
     def add_trigger(self, params):
         if self._module.check_mode:
             self._module.exit_json(changed=True)
         try:
-            results = self._zapi.trigger.create(params)
+            return self._zapi.trigger.create(params)
         except Exception as e:
             self._module.fail_json(msg="Failed to create trigger: %s" % e)
-        return results
 
     def update_trigger(self, params):
         if self._module.check_mode:
             self._module.exit_json(changed=True)
         try:
-            results = self._zapi.trigger.update(params)
+            return self._zapi.trigger.update(params)
         except Exception as e:
             self._module.fail_json(msg="Failed to update trigger: %s" % e)
-        return results
 
     def check_trigger_changed(self, old_trigger):
         try:
@@ -392,10 +347,9 @@ class Trigger(ZabbixBase):
         if self._module.check_mode:
             self._module.exit_json(changed=True)
         try:
-            results = self._zapi.trigger.delete(trigger_id)
+            return self._zapi.trigger.delete(trigger_id)
         except Exception as e:
             self._module.fail_json(msg="Failed to delete trigger: %s" % e)
-        return results
 
 
 def main():
@@ -404,11 +358,21 @@ def main():
         name=dict(type='str', required=True),
         host_name=dict(type='str', required=False),
         template_name=dict(type='str', required=False),
-        params=dict(type='dict', required=False),
+        params=dict(type='dict', required=False, default={}),
         desc=dict(type='str', required=False, aliases=['description']),
-        dependencies=dict(type='list', elements='dict', required=False),
+        dependencies=dict(
+            type='list', 
+            elements='dict', 
+            required=False,
+            options=dict(
+                name=dict(type='str', required=True),
+                host_name=dict(type='str', required=False),
+                template_name=dict(type='str', required=False)
+            )
+        ),
         state=dict(type='str', default="present", choices=['present', 'absent']),
     ))
+    
     module = AnsibleModule(
         argument_spec=argument_spec,
         required_one_of=[
@@ -424,36 +388,36 @@ def main():
     )
 
     name = module.params['name']
-    host_name = module.params['host_name']
-    template_name = module.params['template_name']
-    params = module.params['params']
-    desc = module.params['desc']
-    dependencies = module.params['dependencies']
+    host_name = module.params.get('host_name')
+    template_name = module.params.get('template_name')
+    params = module.params.get('params', {})
+    desc = module.params.get('desc')
+    dependencies = module.params.get('dependencies')
     state = module.params['state']
 
     trigger = Trigger(module)
 
     if state == "absent":
         triggers = trigger.get_triggers(name, host_name, template_name)
-        if len(triggers) == 0:
+        if not triggers:
             module.exit_json(changed=False, result="No trigger to delete.")
         else:
-            delete_ids = []
-            for t in triggers:
-                delete_ids.append(t['triggerid'])
+            delete_ids = [t['triggerid'] for t in triggers]
             results = trigger.delete_trigger(delete_ids)
             module.exit_json(changed=True, result=results)
 
     elif state == "present":
         trigger.sanitize_params(name, params, desc, dependencies)
         triggers = trigger.get_triggers(name, host_name, template_name)
+        
         if 'new_name' in params:
             new_name_trigger = trigger.get_triggers(params['new_name'], host_name, template_name)
-            if len(new_name_trigger) > 0:
+            if new_name_trigger:
                 module.exit_json(changed=False, result=[{'triggerids': [new_name_trigger[0]['triggerid']]}])
-        if len(triggers) == 0:
+                
+        if not triggers:
             if 'new_name' in params:
-                module.fail_json('Cannot rename trigger:  %s is not found' % name)
+                module.fail_json(msg='Cannot rename trigger: %s is not found' % name)
             results = trigger.add_trigger(params)
             module.exit_json(changed=True, result=results)
         else:
@@ -461,13 +425,11 @@ def main():
             changed = False
             for t in triggers:
                 params['triggerid'] = t['triggerid']
-                params.pop('description')
+                params.pop('description', None)
                 if 'new_name' in params:
-                    params['description'] = params['new_name']
-                    params.pop("new_name")
+                    params['description'] = params.pop("new_name")
                 results.append(trigger.update_trigger(params))
-                changed_trigger = trigger.check_trigger_changed(t)
-                if changed_trigger:
+                if trigger.check_trigger_changed(t):
                     changed = True
             module.exit_json(changed=changed, result=results)
 
